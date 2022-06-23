b117668320d1d89870eff05c75973e9c8730345f
Kishore <kishore.k@sliceit.com>
2022-06-23 18:20:04 +0530

    Added basic get and post api

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
 console.log("Creating the app")
 console.log("Will be up soon")
 console.log("Git testing -1")
+=======
+let express =require('express')
+let app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const port=3000
+let name=[]
+
+app.listen(port,()=>{
+    console.log(`Listening to port number ${port}`)
+})
+
+app.get('/',(req,res)=>{
+    res.send('Data: '+name)
+
+})
+app.post('/',(req,res)=>{
+    name.push(req.body.name)
+    res.send(req.body.name+' is be added')
+})
+>>>>>>> ab701ff (Added basic get and post api)
